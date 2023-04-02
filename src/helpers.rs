b53817daba30a97103fbdbad95d7efc2a7c081b4
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-04-02 22:43:06 +0200

    solve! macro

--- src/helpers.rs
+++ src/helpers.rs
@@ -10,23 +10,46 @@ use {
 /// solves and displays pt1 and pt2 of `day`
 pub fn solve_day(day: u8) {
     let filename = format!("./data/inputs/input{day:02}.txt");
+
+    /// the macro takes a `dayXX` submodule and displays its `dayXX::pt1` and `dayXX::pt2` solutions
+    macro_rules! solve {
+        ($dayXX:ident) => {{
+            let (answer1, time1) = solutions::$dayXX::pt1(&filename);
+            let (answer2, time2) = solutions::$dayXX::pt2(&filename);
+
+            println!("Day {day:02}");
+            println!("    part 1: {answer1}, elapsed time: {time1} ms");
+            println!("    part 2: {answer2}, elapsed time: {time2} ms");
+        }};
+    }
+
+    // solving the `day`
     match day {
-        1 => {
-            let (answer1, time1) = solutions::day01::pt1(&filename);
-            let (answer2, time2) = solutions::day01::pt2(&filename);
-
-            println!("Day {day:02}");
-            println!("    part 1: {answer1}, elapsed time: {time1} ms");
-            println!("    part 2: {answer2}, elapsed time: {time2} ms");
-        }
-        2 => {
-            let (answer1, time1) = solutions::day02::pt1(&filename);
-            let (answer2, time2) = solutions::day02::pt2(&filename);
-
-            println!("Day {day:02}");
-            println!("    part 1: {answer1}, elapsed time: {time1} ms");
-            println!("    part 2: {answer2}, elapsed time: {time2} ms");
-        }
+        1 => solve!(day01),
+        2 => solve!(day02),
+        // 3 => solve!(day03),
+        // 4 => solve!(day04),
+        // 5 => solve!(day05),
+        // 6 => solve!(day06),
+        // 7 => solve!(day07),
+        // 8 => solve!(day08),
+        // 9 => solve!(day09),
+        // 10 => solve!(day010),
+        // 11 => solve!(day011),
+        // 12 => solve!(day012),
+        // 13 => solve!(day013),
+        // 14 => solve!(day014),
+        // 15 => solve!(day015),
+        // 16 => solve!(day016),
+        // 17 => solve!(day017),
+        // 18 => solve!(day018),
+        // 19 => solve!(day019),
+        // 20 => solve!(day020),
+        // 21 => solve!(day021),
+        // 22 => solve!(day022),
+        // 23 => solve!(day023),
+        // 24 => solve!(day024),
+        // 25 => solve!(day025),
         _ => println!("Day {day:02}\n    not implemented!"),
     }
 }
