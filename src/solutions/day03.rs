3b7c305733ad900031f0f92d4616ddcb3b5bf2f2
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-04-03 21:37:22 +0200

    day03

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -6,9 +6,6 @@
 
 use {crate::helpers::read_lines, std::time::SystemTime};
 
-const VALUE_SMALL_A: u32 = 'a' as u32;
-const VALUE_BIG_A: u32 = 'A' as u32;
-
 pub fn pt1(filename: &str) -> (u32, u32) {
     let time = SystemTime::now();
 
@@ -27,9 +24,39 @@ pub fn pt1(filename: &str) -> (u32, u32) {
 pub fn pt2(filename: &str) -> (u32, u32) {
     let time = SystemTime::now();
 
+    let mut current_group = Group {
+        first: String::from(""),
+        second: String::from(""),
+        third: String::from(""),
+    };
+
+    let mut current_index = 1;
+
+    let mut answer = 0;
+
+    for line in read_lines(filename).flatten() {
+        match current_index {
+            1 => {
+                current_group.first = line;
+                current_index += 1;
+            }
+            2 => {
+                current_group.second = line;
+                current_index += 1;
+            }
+            // tuple is complete with group, so find the common item and its priority
+            3 => {
+                current_group.third = line;
+                answer += current_group.value_of_common();
+                current_index = 1; // reset counter
+            }
+            _ => panic!("current index not covered"),
+        }
+    }
+
     let time = time.elapsed().unwrap().as_millis() as u32;
 
-    (0, time)
+    (answer, time)
 }
 
 /// represents a Rucksack and the content on its 1st and 2nd compartments
@@ -43,15 +70,7 @@ impl Rucksack {
     fn value_of_common(&self) -> u32 {
         let common = self.find_common();
 
-        if !common.is_alphabetic() {
-            panic!("sack content isn't alhabetic");
-        }
-
-        if common.is_lowercase() {
-            common as u32 - VALUE_SMALL_A + 1
-        } else {
-            common as u32 - VALUE_BIG_A + 27
-        }
+        char_to_priority(common)
     }
 
     /// finds the item present in the first and second compartments of the rucksack
@@ -59,7 +78,7 @@ impl Rucksack {
         self.comp_1
             .chars()
             .find(|c| self.comp_2.contains(c.to_owned()))
-            .unwrap()
+            .expect("no common found")
     }
 
     /// parses a line into a `Rucksack`
@@ -70,6 +89,42 @@ impl Rucksack {
             comp_1: comp_1.to_string(),
             comp_2: comp_2.to_string(),
         }
+    }
+}
+
+struct Group {
+    first: String,
+    second: String,
+    third: String,
+}
+
+impl Group {
+    /// returns the priority value of the common item
+    fn value_of_common(&self) -> u32 {
+        let common = self.find_common();
+
+        char_to_priority(common)
+    }
+
+    /// finds the common item that all elfs in the group have
+    fn find_common(&self) -> char {
+        self.first
+            .chars()
+            .find(|c| self.second.contains(c.to_owned()) && self.third.contains(c.to_owned()))
+            .expect("no common found")
+    }
+}
+
+/// converts a char into its correnponding priority
+fn char_to_priority(c: char) -> u32 {
+    if !c.is_alphabetic() {
+        panic!("can't convert common to priority. char is not alphabetic");
+    } else if c.is_lowercase() {
+        // 'a' char has a value of 97. this converts 'a' to 1 and so forth
+        c as u32 - 97 + 1
+    } else {
+        // 'A' char has a value of 65. this converts 'A' to 27 and so forth
+        c as u32 - 65 + 27
     }
 }
 
@@ -87,11 +142,11 @@ mod tests {
         assert_eq!(157, answer);
     }
 
-    // #[test]
-    // fn pt02() {
-    //     let (answer, _) = pt2(FILENAME);
-    //     assert_eq!(, answer);
-    // }
+    #[test]
+    fn pt02() {
+        let (answer, _) = pt2(FILENAME);
+        assert_eq!(70, answer);
+    }
 
     #[test]
     fn test_find_common() {
